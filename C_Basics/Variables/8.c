f1907851ac2045c7fc89e921140863a871ecbaee
Ashutosh Kumar Singh <ashutoshsingh1150@gmail.com>
2024-05-18 01:39:31 +0530

    8

---
+++ C_Basics/Variables/8.c
@@ -0,0 +1,34 @@
+ #include<stdio.h>
+
+int main()
+{
+int SP;
+int Profit;
+int n;
+int CP;
+int d;
+
+printf("Selling Price is:");
+scanf("%d",&SP);
+
+printf("Enter profit:");
+scanf("%d",&Profit);
+
+printf("Enter the number of items");
+scanf("%d",&n);
+
+CP=SP-Profit;
+
+d=CP/n;
+
+printf("%d",d);
+
+return 0;
+
+
+
+
+
+
+
+}
