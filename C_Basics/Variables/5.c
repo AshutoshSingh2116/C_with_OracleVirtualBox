3b13eae551e2b15d3878a1224b99cfbc74f8ede8
Ashutosh Kumar Singh <ashutoshsingh1150@gmail.com>
2024-05-18 01:38:56 +0530

    5

---
+++ C_Basics/Variables/5.c
@@ -0,0 +1,27 @@
+  #include<stdio.h>
+ 
+  int main()
+  {
+  float l;
+  float b;
+  float perimeter;
+  float area;
+  
+  printf("Enter the length:");
+  scanf("%f", &l);
+  
+   printf("Enter the breadth:");
+   scanf("%f", &b);
+
+
+  area=l*b;
+  
+  perimeter=2*(l+b);
+  
+  printf("The area is:%f",area);
+  printf("The perimeter is:%f",perimeter);
+  
+  return 0;
+  
+   }
+
