3c8263f846f5b919629613a4d42eca454bd85f48
Ashutosh Kumar Singh <ashutoshsingh1150@gmail.com>
2024-05-18 01:38:46 +0530

    4

---
+++ C_Basics/Variables/4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+
+{
+int a,b,c,d;
+int add;
+float average;
+printf("Take 4 intrgers from user:");
+scanf("%d%d%d%d",&a,&b,&c,&d);
+
+add=a+b+c+d;
+
+average=add/4.0;
+
+printf("%f",average);
+
+return 0;
+
+}
