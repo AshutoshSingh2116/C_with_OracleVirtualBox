ffd6d0f467e4f0b38ebc82074dc893107505ef58
Ashutosh Kumar Singh <ashutoshsingh1150@gmail.com>
2024-05-18 18:18:47 +0530

    3

--- C_Basics/Arithmetic_Operators/3.c
+++ C_Basics/Arithmetic_Operators/3.c
@@ -1,3 +1,5 @@
+//Take three input integers x,y and z. Write a program to rotate their values such that, x has the value of y, y has the value of z and z has the value of x.
+
 #include<stdio.h>
 int main()
 {
