35bbb60f0d7878e860d68c5be689e589b5ab5e0c
Ashutosh Kumar Singh <ashutoshsingh1150@gmail.com>
2024-05-20 14:32:02 +0530

    6

--- C_Basics/Variables/6.c
+++ C_Basics/Variables/6.c
@@ -2,21 +2,21 @@
   
   int main()
   {
-  float s;
-  float area;
-  float perimeter;
+  	float s;
+  	float area;
+  	float perimeter;
    
-  printf("Enter the side:");
-  scanf("%f", &s);
+  	printf("Enter the side:");
+  	scanf("%f", &s);
   
-  area=s*s;
+  	area=s*s;
   
-  perimeter=4*s;
+  	perimeter=4*s;
   
-  printf("The area is:%f",area);
-  printf("The perimeter is:%f",perimeter);
+  	printf("The area is:%f",area);
+  	printf("The perimeter is:%f",perimeter);
   
-  return 0;
+  	return 0;
   
   }
 
