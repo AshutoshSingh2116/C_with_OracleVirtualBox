f09f83e909ea3beea37c2b8c1331a4980216c1ef
Ashutosh Kumar Singh <ashutoshsingh1150@gmail.com>
2024-05-20 14:32:11 +0530

    7

--- C_Basics/Variables/7.c
+++ C_Basics/Variables/7.c
@@ -2,20 +2,20 @@
 
 int main()
 {
-float radius;
-float area;
-float perimeter;
+	float radius;
+	float area;
+	float perimeter;
 
-printf("Enter the radius:");
-scanf("%f", &radius);
+	printf("Enter the radius:");
+	scanf("%f", &radius);
 
-area=3.14*radius*radius;
+	area=3.14*radius*radius;
 
-perimeter=2*3.14*radius;
+	perimeter=2*3.14*radius;
 
-printf("The area is:%f",area);
-printf("The perimeter is:%f",perimeter);
+	printf("The area is:%f",area);
+	printf("The perimeter is:%f",perimeter);
 
-return 0;
+	return 0;
 
 }
