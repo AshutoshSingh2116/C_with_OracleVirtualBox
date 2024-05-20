fbb6faa6ca5a2fffda098a8fc28f12274b029ec6
Ashutosh Kumar Singh <ashutoshsingh1150@gmail.com>
2024-05-20 14:32:18 +0530

    8

--- C_Basics/Variables/8.c
+++ C_Basics/Variables/8.c
@@ -2,28 +2,28 @@
 
 int main()
 {
-int SP;
-int Profit;
-int n;
-int CP;
-int d;
+	int SP;
+	int Profit;
+	int n;
+	int CP;
+	int d;
 
-printf("Selling Price is:");
-scanf("%d",&SP);
+	printf("Selling Price is:");
+	scanf("%d",&SP);
 
-printf("Enter profit:");
-scanf("%d",&Profit);
+	printf("Enter profit:");
+	scanf("%d",&Profit);
 
-printf("Enter the number of items");
-scanf("%d",&n);
+	printf("Enter the number of items");
+	scanf("%d",&n);
 
-CP=SP-Profit;
+	CP=SP-Profit;
 
-d=CP/n;
+	d=CP/n;
 
-printf("%d",d);
+	printf("%d",d);
 
-return 0;
+	return 0;
 
 
 
