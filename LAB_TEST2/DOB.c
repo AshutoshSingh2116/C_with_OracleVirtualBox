7ef9c34de130e4bb556f3c797a94fe763a0f7df1
Ashutosh Kumar Singh <ashutoshsingh1150@gmail.com>
2024-05-22 18:11:13 +0530

    DOB

--- LAB_TEST2/DOB.c
+++ LAB_TEST2/DOB.c
@@ -1,3 +1,18 @@
+/*Question: Read the dates of births of two candidate's user into 3 different variables each, and print which date born person is the older of the two.
+            If date is 28-01-2000, then store 28 in day variable, 1 in month variable and 2000 in year variable.
+            Compare two years, two months and two days separately, based on how we normally compare dates to see which is old and which is new.
+  
+  eg: DOB-1: 28-1-2000
+      DOB-2: 28-2-2001
+      Output: Person born on 28-01-2000 is older.       */
+
+
+
+
+
+
+
+
 #include<stdio.h>
 int main()
 {
@@ -56,4 +71,6 @@ return 0;
 
 
 
-
+/* eg: DOB-1: 28-1-2000
+       DOB-2: 28-2-2001
+       Output: Person born on 28-01-2000 is older.    */
